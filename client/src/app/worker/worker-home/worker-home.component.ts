f43160864ad2a34a4533e3b531e285fecf161f72
Hbib24 <habib.bekir@gmail.com>
2020-12-19 18:13:10 +0100

    worker home

--- client/src/app/worker/worker-home/worker-home.component.ts
+++ client/src/app/worker/worker-home/worker-home.component.ts
@@ -22,8 +22,8 @@ export class WorkerHomeComponent {
       }
 
       return [
-        { title: 'Card 1', cols: 2, rows: 1 },
-        { title: 'Card 2', cols: 2, rows: 1 },
+        { title: 'Card 1', cols: 1, rows: 1 },
+        { title: 'Card 2', cols: 1, rows: 1 },
         { title: 'Card 3', cols: 1, rows: 1 },
         { title: 'Card 4', cols: 1, rows: 1 },
       ];
